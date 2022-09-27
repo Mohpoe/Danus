0a2dafc0b5f514bf2964fa2d64b0975e7713e8af
Maringngerrang <nuzumaki247@gmail.com>
2022-09-27 20:53:55 +0800

    Atur agar kolom gambar_barang langsung ke path nya

--- app/Models/Barang.php
+++ app/Models/Barang.php
@@ -10,6 +10,15 @@ class Barang extends Model
   use HasFactory;
   protected $guarded = [];
 
+  public function getGambarBarangAttribute($nilai)
+  {
+    if (!empty($nilai)) {
+      return asset('images/barang/' . $nilai);
+    } else {
+      return asset('images/barang/dummy.jpg');
+    }
+  }
+
   // public function barang()
   // {
   //   return $this->hasMany('App\Models\Riwayat', 'barang_id');
