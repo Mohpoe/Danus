9dc98af970c3394a033c89e61801c0ed7fd10f1c
Maringngerrang <nuzumaki247@gmail.com>
2022-09-27 20:58:51 +0800

    input kata_sandi => enkripsi & output foto => path

--- app/Models/Pengguna.php
+++ app/Models/Pengguna.php
@@ -8,14 +8,29 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class Pengguna extends Authenticatable
 {
   use HasFactory;
-  protected $fillable = [
-    'nama_pengguna',
-    'kata_sandi',
-    'nama_lengkap',
-  ];
+  protected $guarded = [];
+  // protected $fillable = [
+  //   'nama_pengguna',
+  //   'kata_sandi',
+  //   'nama_lengkap',
+  // ];
   protected $hidden = [
     'kata_sandi',
   ];
+
+  public function setKataSandiAttribute($nilai)
+  {
+    $this->attributes['kata_sandi'] = bcrypt($nilai);
+  }
+
+  public function getFotoAttribute($nilai)
+  {
+    if (!empty($nilai)) {
+      return asset('images/pengguna/' . $nilai);
+    } else {
+      return asset('images/pengguna/ava.webp');
+    }
+  }
 
   // public function pengguna()
   // {
